f0063fc1d4595ed81a1ffce0fec45388ba5aac79
Maganth Seetharaman <mahanthathreyee@gmail.com>
2019-03-19 13:53:55 +0000

    Removed auto scrolling due no cross browser support, changed preloader image width | updated names in members

--- scroll.js
+++ scroll.js
@@ -2,30 +2,23 @@ var lastpos = 0;
 document.addEventListener('scroll', () => {   
     let scroll_pos = $(this).scrollTop();
     if(scroll_pos - lastpos > 0
-        && scroll_pos/$('.starting-section').height() * 100 > 0
-        && scroll_pos <= $('.container').offset().top){
-            window.scrollTo(0, $('.container').offset().top+1);   
-            if($(this).scrollTop() == $('.container').offset().top){
-                anime({
-                    targets: '.sticky-header',
-                    height: '100px',
-                    duration: 50,
-                    easing: 'easeOutQuint'
-                });        
-                $('.sticky-header > img').css({height:"100px"});
-            }
+        && scroll_pos/$('.starting-section').height() * 100 > 60){
+            anime({
+                targets: '.sticky-header',
+                height: '100px',
+                duration: 50,
+                easing: 'easeOutQuint'
+            });        
+            $('.sticky-header > img').css({height:"100px"});
     }
     else if(scroll_pos - lastpos < 0
-        && scroll_pos/$('.starting-section').height() * 100 < 100){
+        && scroll_pos/$('.starting-section').height() * 100 < 60){
             $('.sticky-header > img').css({height:"0"}); 
             anime({
                 targets: '.sticky-header',
                 height: '0',
                 duration: 50,
                 easing: 'easeOutQuint',
-                complete: ()=>{
-                    window.scrollTo(0, 0);                 
-                }
             });  
     }
 
@@ -33,9 +26,9 @@ document.addEventListener('scroll', () => {
 });
 
 function scroll_down_page(){
-    window.scrollTo(0, $('.container').offset().top);
+    $("html,body").animate({scrollTop: $('.container').offset().top});               
 }
 
 function scroll_up_page(){
-    window.scrollTo(0, 0);
+    $("html,body").animate({scrollTop: 0});                   
 }
